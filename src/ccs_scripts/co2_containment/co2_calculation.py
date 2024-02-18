d1d3ac4189306d6f446e7057f1a848267de335a2
Audun Sektnan <99661468+AudunSektnanNR@users.noreply.github.com>
2024-02-18 10:35:20 +0000

    Latest from old develop-branch (#92)
    * Add more specific message is some property lacks
    
    * Minor changes
    
    * Remove trailing whitespace
    
    * Small format changes
    
    ---------
    
    Co-authored-by: Jorge Sicacha <sicacha@nr.no>
    Co-authored-by: jorgesicachanr <114475076+jorgesicachanr@users.noreply.github.com>

--- src/ccs_scripts/co2_containment/co2_calculation.py
+++ src/ccs_scripts/co2_containment/co2_calculation.py
@@ -699,13 +699,33 @@ def _calculate_co2_data_from_source_data(
             source = "PFlotran"
         elif _is_subset(["RPORV", "BGAS", "BWAT", "XMF2", "YMF2"], active_props):
             source = "Eclipse"
-        else:
+        elif any(
+            prop in ["PORV", "DGAS", "DWAT", "AMFG", "YMFG"] for prop in active_props
+        ):
+            missing_props = [
+                x
+                for x in ["PORV", "DGAS", "DWAT", "AMFG", "YMFG"]
+                if x not in active_props
+            ]
             error_text = "Lacking some required properties to compute CO2 mass/volume"
+            error_text += "\nMissing: "
+            error_text += ", ".join([property for property in missing_props])
             raise RuntimeError(error_text)
-    else:
-        error_text = "Lacking some required properties to compute CO2 mass/volume"
-        error_text += "\nMissing: SGAS"
-        raise RuntimeError(error_text)
+        elif any(
+            prop in ["RPORV", "BGAS", "BWAT", "XMF2", "YMF2"] for prop in active_props
+        ):
+            missing_props = [
+                x
+                for x in ["RPORV", "BGAS", "BWAT", "XMF2", "YMF2"]
+                if x not in active_props
+            ]
+            error_text = "Lacking some required properties to compute CO2 mass/volume"
+            error_text += "\nMissing: "
+            error_text += ", ".join([property for property in missing_props])
+            raise RuntimeError(error_text)
+        else:
+            error_text = "Lacking all required properties to compute CO2 mass/volume"
+            raise RuntimeError(error_text)
 
     if calc_type in (CalculationType.ACTUAL_VOLUME, CalculationType.MASS):
         if source == "PFlotran":
