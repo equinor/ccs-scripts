8523140d9ddb542c68b31bbe7ae383e71745eca2
root <root@NR-JKY93M3>
2024-06-27 16:03:19 +0200

    Check test

--- src/ccs_scripts/co2_mass_maps/co2_mass_maps.py
+++ src/ccs_scripts/co2_mass_maps/co2_mass_maps.py
@@ -61,6 +61,8 @@ def generate_co2_mass_maps(config_):
     grid_file = config_.input.grid
     zone_info = {"source": None, "zranges": None}
     region_info = {"source": None, "property_name": None}
+    print("This is relevant properties")
+    print(RELEVANT_PROPERTIES)
     co2_data = calculate_co2(
         grid_file=grid_file,
         unrst_file=co2_mass_settings.unrst_source,
@@ -217,8 +219,6 @@ def main(arguments=None):
     if config_.co2_mass_settings is None:
         raise ValueError("CO2 mass computation needs co2_mass_settings as input")
 
-    print("This is relevant properties")
-    print(RELEVANT_PROPERTIES)
     generate_co2_mass_maps(config_)
 
 
