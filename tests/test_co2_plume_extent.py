d1003aaa41bed991f63218a8937fc14b5cb9e91b
AudunSektnanNR <sek@nr.no>
2024-06-27 11:24:58 +0200

    CCS-129: Done fixing tests.

--- tests/test_co2_plume_extent.py
+++ tests/test_co2_plume_extent.py
@@ -396,8 +396,6 @@ def test_plume_extent_pflotran_using_well_name(mocker):
 
     df = df.sort_values("date")
     df_answer = df_answer.sort_values("date")
-    print(df)
-    print(df_answer)
     pandas.testing.assert_frame_equal(df, df_answer)
 
 
@@ -498,6 +496,4 @@ def test_yaml_file_pflotran(mocker):
 
     df = df.sort_values("date")
     df_answer = df_answer.sort_values("date")
-    print(df)
-    print(df_answer)
     pandas.testing.assert_frame_equal(df, df_answer)
