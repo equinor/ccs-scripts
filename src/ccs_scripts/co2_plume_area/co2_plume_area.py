f2d5ee17cf679cde312ca06c2b681d6053b0d32b
Audun Sektnan <sek@nr.no>
2024-09-25 09:28:57 +0200

    Start dev version of v0.8.0

--- src/ccs_scripts/co2_plume_area/co2_plume_area.py
+++ src/ccs_scripts/co2_plume_area/co2_plume_area.py
@@ -180,8 +180,8 @@ def _read_args() -> Tuple[str, str]:
 
 
 def _log_input_configuration(input_path: str, output_path: str) -> None:
-    version = "v0.7.0"
-    is_dev_version = False
+    version = "v0.8.0"
+    is_dev_version = True
     if is_dev_version:
         version += "_dev"
         try:
--- src/ccs_scripts/co2_plume_extent/co2_plume_extent.py
+++ src/ccs_scripts/co2_plume_extent/co2_plume_extent.py
@@ -517,8 +517,8 @@ def _setup_log_configuration(arguments: argparse.Namespace) -> None:
 
 
 def _log_input_configuration(arguments: argparse.Namespace) -> None:
-    version = "v0.7.0"
-    is_dev_version = False
+    version = "v0.8.0"
+    is_dev_version = True
     if is_dev_version:
         version += "_dev"
         try:
