6e0b7682dd512e6208cc2510b1e6f05e080adcdb
Audun Sektnan <sek@nr.no>
2024-10-24 14:50:55 +0200

    CCS-189: Remove some temp code.

--- src/ccs_scripts/co2_plume_tracking/co2_plume_tracking.py
+++ src/ccs_scripts/co2_plume_tracking/co2_plume_tracking.py
@@ -224,29 +224,21 @@ def calculate_all_plume_groups(
     threshold_aqueous: float,
     inj_wells: List[InjectionWellData],
 ) -> Tuple[List[List[str]], Optional[List[List[str]]], Optional[str]]:
-    # thresholds_gas = [0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.90]
-    thresholds_gas = [threshold_gas]
-    for x in thresholds_gas:
-        print(f"\n\nthreshold_gas: {x}")
-        pg_prop_sgas = calculate_plume_groups(
-            "SGAS",
-            x,
+    pg_prop_sgas = calculate_plume_groups(
+        "SGAS",
+        threshold_gas,
+        unrst,
+        grid,
+        inj_wells,
+    )
+    if "AMFG" in unrst:
+        pg_prop_amfg = calculate_plume_groups(
+            "AMFG",
+            threshold_aqueous,
             unrst,
             grid,
             inj_wells,
         )
-    if "AMFG" in unrst:
-        # thresholds_aq = [0.00001, 0.00005, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0008, 0.001, 0.0012, 0.0014, 0.0016, 0.0018, 0.0020, 0.003, 0.004, 0.005, 0.007, 0.01, 0.02, 0.05, 0.1, 0.5]
-        thresholds_aq = [threshold_aqueous]
-        for x in thresholds_aq:
-            print(f"\n\nthreshold_aq: {x}")
-            pg_prop_amfg = calculate_plume_groups(
-                "AMFG",
-                x,
-                unrst,
-                grid,
-                inj_wells,
-            )
         amfg_key = "AMFG"
     elif "XMF2" in unrst:
         pg_prop_amfg = calculate_plume_groups(
