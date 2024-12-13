de7b4d72a5c974b1fa18481be315778f9e67ca22
Jorge Sicacha <sicacha@nr.no>
2024-12-13 11:18:51 +0100

    Changes in _distribute_config_property

--- src/ccs_scripts/aggregate/grid3d_aggregate_map.py
+++ src/ccs_scripts/aggregate/grid3d_aggregate_map.py
@@ -207,57 +207,59 @@ def generate_from_config(config: _config.RootConfig):
 
 
 def _distribute_config_property(config_: _config.RootConfig):
-    if config_.input.properties is not None:
-        if isinstance(config_.input.properties[0].name, list):
-            tmp_props = config_.input.properties.pop()
-            if isinstance(tmp_props.lower_threshold, list) and len(
+    if config_.input.properties is None:
+        return
+    if not isinstance(config_.input.properties[0].name, list):
+        return
+    tmp_props = config_.input.properties.pop()
+    if isinstance(tmp_props.lower_threshold, list) and len(
+        tmp_props.name
+    ) == len(tmp_props.lower_threshold):
+        config_.input.properties.extend(
+            [
+                _config.Property(tmp_props.source, name, threshold)
+                for name, threshold in zip(
+                    tmp_props.name, tmp_props.lower_threshold
+                )
+            ]
+        )
+    elif isinstance(tmp_props.lower_threshold, float) or (
+        isinstance(tmp_props.lower_threshold, list)
+        and len(tmp_props.lower_threshold) == 1
+    ):
+        logging.info(
+            f"Only one value of threshold for {str(len(tmp_props.name))}."
+            f"properties. The same threshold will be assumed for all the"
+            f"properties."
+        )
+        if (
+            isinstance(tmp_props.lower_threshold, list)
+            and len(tmp_props.lower_threshold) == 1
+        ):
+            tmp_props.lower_threshold = tmp_props.lower_threshold[0] * len(
                 tmp_props.name
-            ) == len(tmp_props.lower_threshold):
-                config_.input.properties.extend(
-                    [
-                        _config.Property(tmp_props.source, name, threshold)
-                        for name, threshold in zip(
-                            tmp_props.name, tmp_props.lower_threshold
-                        )
-                    ]
+            )
+        else:
+            tmp_props.lower_threshold = [tmp_props.lower_threshold] * len(
+                tmp_props.name
+            )
+        config_.input.properties.extend(
+            [
+                _config.Property(tmp_props.source, name, threshold)
+                for name, threshold in zip(
+                    tmp_props.name, tmp_props.lower_threshold
                 )
-            elif isinstance(tmp_props.lower_threshold, float) or (
-                isinstance(tmp_props.lower_threshold, list)
-                and len(tmp_props.lower_threshold) == 1
-            ):
-                logging.info(
-                    f"Only one value of threshold for {str(len(tmp_props.name))}."
-                    f"properties. The same threshold will be assumed for all the"
-                    f"properties."
-                )
-                if (
-                    isinstance(tmp_props.lower_threshold, list)
-                    and len(tmp_props.lower_threshold) == 1
-                ):
-                    tmp_props.lower_threshold = tmp_props.lower_threshold[0] * len(
-                        tmp_props.name
-                    )
-                else:
-                    tmp_props.lower_threshold = [tmp_props.lower_threshold] * len(
-                        tmp_props.name
-                    )
-                config_.input.properties.extend(
-                    [
-                        _config.Property(tmp_props.source, name, threshold)
-                        for name, threshold in zip(
-                            tmp_props.name, tmp_props.lower_threshold
-                        )
-                    ]
-                )
-            else:
-                error_text = (
-                    f"{str(len(tmp_props.lower_threshold))} values of co2_threshold"
-                    f"provided, but {str(len(tmp_props.name))} properties in config"
-                    f" file input. Fix the amount of values in co2_threshold or"
-                    f"the amount of properties in config file"
-                )
-                raise Exception(error_text)
-    return config_
+            ]
+        )
+    else:
+        error_text = (
+            f"{str(len(tmp_props.lower_threshold))} values of co2_threshold"
+            f"provided, but {str(len(tmp_props.name))} properties in config"
+            f" file input. Fix the amount of values in co2_threshold or"
+            f"the amount of properties in config file"
+        )
+        raise Exception(error_text)
+    return
 
 
 def main(arguments=None):
