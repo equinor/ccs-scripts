db7deb57fe2808693bcf77073b1d2f7fcd5c16e6
Audun Sektnan <sek@nr.no>
2025-10-17 10:55:59 +0200

    Start dev version 0.12.0.

--- src/ccs_scripts/co2_containment/co2_containment.py
+++ src/ccs_scripts/co2_containment/co2_containment.py
@@ -640,8 +640,8 @@ def log_input_configuration(args: argparse.Namespace) -> None:
     """
     Log the provided input
     """
-    version = "v0.11.0"
-    is_dev_version = False
+    version = "v0.12.0"
+    is_dev_version = True
     if is_dev_version:
         version += "_dev"
         try:
--- src/ccs_scripts/co2_plume_area/co2_plume_area.py
+++ src/ccs_scripts/co2_plume_area/co2_plume_area.py
@@ -189,8 +189,8 @@ def _read_args() -> Tuple[str, str]:
 
 
 def _log_input_configuration(input_path: str, output_path: str) -> None:
-    version = "v0.11.0"
-    is_dev_version = False
+    version = "v0.12.0"
+    is_dev_version = True
     if is_dev_version:
         version += "_dev"
         try:
